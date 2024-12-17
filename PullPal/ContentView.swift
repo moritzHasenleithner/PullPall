c6eb6320a0d62f99a010ac6f8c6c9a4cde74471b
Mhase <s2210237008@fhooe.at>
2024-12-17 07:18:33 +0100

    Initial Commit

---
+++ PullPal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PullPal
+//
+//  Created by Moritz Hasenleithner on 17.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PullPal/PullPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullPalApp.swift
+//  PullPal
+//
+//  Created by Moritz Hasenleithner on 17.12.24.
+//
+
+import SwiftUI
+
+@main
+struct PullPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
